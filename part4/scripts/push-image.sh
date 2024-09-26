99aa199130aba30a2e8d61d0dca728aca1da91da
ShubhUniyal <162271076+ShubhUniyal@users.noreply.github.com>
2024-09-27 03:02:21 +1000

    terraform updated files

---
+++ part4/scripts/push-image.sh
@@ -0,0 +1,4 @@
+docker tag part4-book_catalog uniyalapp123.azurecr.io/book_catalog:v2
+docker tag part4-inventory_management uniyalapp123.azurecr.io/inventory_management:v2
+docker push uniyalapp123.azurecr.io/book_catalog:v2
+docker push uniyalapp123.azurecr.io/inventory_management:v2
